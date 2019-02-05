ad08762543fa9dcb336a8fe0b13121fc85ade641
Jorge Almeida Junior <jorgealmeidajunior@gmail.com>
2019-02-05 18:18:59 -0200

    Dependency puppeteer installed.

--- src/hotels.controller.ts
+++ src/hotels.controller.ts
@@ -7,6 +7,11 @@ export class HotelsController {
   
   constructor(private readonly hotelsService: HotelsService) {}
 
+  @Get()
+  buscar() {
+    return this.hotelsService.search();
+  }
+
   @Post('buscar')
   search(@Body() searchHotelsDto) {
     return this.hotelsService.search();
--- src/hotels.service.ts
+++ src/hotels.service.ts
@@ -1,14 +1,25 @@
 
 import { Injectable } from '@nestjs/common'
 
+import * as puppeteer from 'puppeteer'
+
 @Injectable()
 export class HotelsService {
 
-  search() {
+  async search() {
     const checkInDate = '05022019'
-    const checkOutDate = '27022019';
-    let requestUrl = `https://myreservations.omnibees.com/chain.aspx?c=2983&lang=pt-BR&ad=2&_ga=2.171262688.1724825935.1549395965-1084404608.1549395965&version=MyReservation&sid=ab4a7cf1-3efe-4a27-954f-cf84dfe8a4f3#/hotel=&hotelname=&CheckIn=${checkInDate}&CheckOut=${checkOutDate}&Code=&group_code=&loyality_card=&NRooms=1&ad=2&ch=0&ag=-`;
+    const checkOutDate = '27022019'
+    let requestUrl = `https://myreservations.omnibees.com/chain.aspx?c=2983&lang=pt-BR&ad=2&_ga=2.171262688.1724825935.1549395965-1084404608.1549395965&version=MyReservation&sid=ab4a7cf1-3efe-4a27-954f-cf84dfe8a4f3#/hotel=&hotelname=&CheckIn=${checkInDate}&CheckOut=${checkOutDate}&Code=&group_code=&loyality_card=&NRooms=1&ad=2&ch=0&ag=-`
     
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+
+    await page.goto(requestUrl)
+
+    // code for scraping
+
+    await browser.close()
+
     return {};
   }
   
